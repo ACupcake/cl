db47656ae37509cda855d999bf740d527cdc907a
Nasser <nasser-silva@hotmail.com>
2023-05-01 12:30:47 -0300

    Add app routers and colors

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,26 @@
 import './App.css'
+import SignUp from './pages/Signup'
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import Home from './pages/Home';
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <SignUp />
+  },
+  {
+    path: "/home",
+    element: <Home />
+  },
+]);
 
 function App() {
   return (
     <>
-
+      <RouterProvider router={router} />
     </>
   )
 }
