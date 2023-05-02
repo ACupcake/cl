e820d5c9558b8e1e8ab10c5976e62e00ceb23acd
Nasser <nasser-silva@hotmail.com>
2023-05-01 23:02:04 -0300

    Add post component

---
+++ src/components/Post.tsx
@@ -0,0 +1,64 @@
+import styles from './Post.module.css'
+import DeleteModal from './DeleteModal';
+import EditModal from './EditModal';
+import { IPost } from '../types/types';
+
+function Post({ id, content, created_datetime, title, username }: IPost) {
+
+    function timeSince(date: Date) {
+        const currMinusLastDate = (Number(new Date()) - Number(date))
+
+        const seconds = Math.floor(currMinusLastDate / 1000);
+
+        let interval = seconds / 31536000;
+
+        if (interval > 1) {
+            return Math.floor(interval) + " years";
+        }
+        interval = seconds / 2592000;
+        if (interval > 1) {
+            return Math.floor(interval) + " months";
+        }
+        interval = seconds / 86400;
+        if (interval > 1) {
+            return Math.floor(interval) + " days";
+        }
+        interval = seconds / 3600;
+        if (interval > 1) {
+            return Math.floor(interval) + " hours";
+        }
+        interval = seconds / 60;
+        if (interval > 1) {
+            return Math.floor(interval) + " minutes";
+        }
+        return Math.floor(seconds) + " seconds";
+    }
+
+    function timePassed() {
+        const date = new Date(created_datetime)
+        return timeSince(new Date(date)) + " ago";
+    }
+
+    return (
+        <>
+            <div className={styles.container}>
+                <div className={styles.header}>
+                    <p className={styles.title}>{title}</p>
+                    <div className={styles.menu}>
+                        <DeleteModal id={id} />
+                        <EditModal id={id} />
+                    </div>
+                </div>
+                <div className={styles.content}>
+                    <div className={styles.postInfo}>
+                        <p className={styles.author}>@{username}</p>
+                        <p>{timePassed()}</p>
+                    </div>
+                    <p className={styles.textContent}>{content}</p>
+                </div>
+            </div >
+        </>
+    )
+}
+
+export default Post
