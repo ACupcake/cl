372f110f962229105e9f5b7b6da926b2f8dda45d
Nasser <nasser-silva@hotmail.com>
2023-05-02 17:41:33 -0300

    Change to localurl and config cors

--- frontend/src/api/api.ts
+++ frontend/src/api/api.ts
@@ -1,10 +1,11 @@
 import axios from "axios";
 import { IApiPatch, IApiPost } from "../types/types";
 
-const apiBase = "http://dev.codeleap.co.uk/"; 
+const codeleapAPI = "http://dev.codeleap.co.uk/careers";
+const ownAPI = "http://127.0.0.1:8000/api/v1/post";
 
 const apiInstance = axios.create({
-    baseURL: apiBase,
+    baseURL: ownAPI,
     timeout: 1000,
   });
 
@@ -14,28 +15,28 @@ const api = {
         if (next) {
             response = await axios.get(next);
         } else {
-            response = await apiInstance.get('/careers/')
+            response = await apiInstance.get('/')
         }
         return response
     },
     get: async (id:number) => {
-        const response = await apiInstance.get('/careers/' + id)
+        const response = await apiInstance.get('/' + id)
         return response
     },
     post: async (data: IApiPost) => {
-        const response = await apiInstance.post('/careers/', data)
+        const response = await apiInstance.post('/', data)
         return response;
     },
     put: async (data: IApiPatch, id:number) => {
-        const response = await apiInstance.put('/careers/' + id + '/', data)
+        const response = await apiInstance.put('/' + id + '/', data)
         return response;
     },
     patch: async (data: IApiPatch, id:number) => {
-        const response = await apiInstance.patch('/careers/' + id + '/', data)
+        const response = await apiInstance.patch('/' + id + '/', data)
         return response;
     },
     delete: async (id:number) => {
-        const response = await apiInstance.delete('/careers/' + id + '/')
+        const response = await apiInstance.delete('/' + id + '/')
         return response;
     }
 }
