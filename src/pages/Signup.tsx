461b0a4761abe9515fc66f0024e3ced7ed8a27cd
Nasser <nasser-silva@hotmail.com>
2023-05-01 23:02:54 -0300

    Add redux to handle username

--- src/pages/Signup.tsx
+++ src/pages/Signup.tsx
@@ -3,15 +3,24 @@ import Button from '../components/Button'
 import Input from '../components/Input'
 import styles from './Signup.module.css'
 import { useNavigate } from "react-router-dom";
+import { useDispatch } from 'react-redux';
+import { changeUsername } from '../redux/slice';
 
 function SignUp() {
     const [username, setUsername] = useState<string>("");
     const navigate = useNavigate();
+    const dispatch = useDispatch()
 
     const enterButton = () => {
         if (username.length > 0) {
             navigate("/home");
         }
+    }
+
+    const handleUsernameChange = (e: any) => {
+        const currName = e.target.value || "";
+        setUsername(currName)
+        dispatch(changeUsername(currName))
     }
 
     return (
@@ -21,7 +30,7 @@ function SignUp() {
             </div>
             <div className={styles.inputContent}>
                 <p>Please enter your username</p>
-                <Input placeholder="Jonh Doe" onChange={(e) => setUsername(e.target.value || "")} />
+                <Input placeholder="Jonh Doe" onChange={(e) => handleUsernameChange(e)} />
             </div>
             <div className={styles.enterButton}>
                 <Button
